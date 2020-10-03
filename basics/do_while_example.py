9c288eff65661df7bdf9c2075a72ecef66393d21
shellhub <shellhub.me@gmail.com>
2020-10-03 22:15:38 +0800

    do-while example

---
+++ basics/do_while_example.py
@@ -0,0 +1,26 @@
+def main() -> None:
+    """
+    do-while example.
+
+    >>> i = 1
+    >>> while True:
+    ...     print(i, end='')
+    ...     i += 1
+    ...     if i == 6:
+    ...         break
+    12345
+
+    >>> i = 5
+    >>> while True:
+    ...     print(i, end='')
+    ...     i -= 1
+    ...     if i == 0:
+    ...         break
+    54321
+    """
+
+
+if __name__ == '__main__':
+    from doctest import testmod
+
+    testmod()
