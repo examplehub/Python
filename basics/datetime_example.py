28a0f61a7c0d722e9d83bc68ac69da6501b36c45
shellhub <shellhub.me@gmail.com>
2020-10-04 01:35:59 +0800

    add date

---
+++ basics/datetime_example.py
@@ -0,0 +1,18 @@
+def main() -> None:
+    """
+    >>> import datetime
+    >>> year = datetime.datetime.now().year
+    >>> year >= 2020
+    True
+
+    >>> time = datetime.datetime(2100, 10, 5)
+    >>> str(time)
+    '2100-10-05 00:00:00'
+    """
+    pass
+
+
+if __name__ == '__main__':
+    from doctest import testmod
+
+    testmod()
