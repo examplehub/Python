b7c1cae668dc4ecb283bf2a54bf65d58ed388a9f
shellhub <shellhub.me@gmail.com>
2020-10-30 16:52:47 +0800

    update project structure

--- basics/for_example.py
+++ basics/for_example.py
@@ -44,6 +44,16 @@ def main() -> None:
 
     >>> sum(i for i in range(1, 101))
     5050
+
+    >>> for i in range(1, 6):
+    ...     for _ in range(1, i + 1):
+    ...         print("*", end='')
+    ...     print()
+    *
+    **
+    ***
+    ****
+    *****
     """
 
 
