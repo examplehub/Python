b75b63a35ff6107a235782a85996c72db339af75
Du Yuanchao <shellhub.me@gmail.com>
2020-11-02 09:59:14 +0800

    Development (#17)
    * reverse string
    
    * Formatted with psf/black
    
    * remove whitespace
    
    * Formatted with psf/black
    
    * format code
    
    * Formatted with psf/black
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ strings/remove_whitespace.py
@@ -0,0 +1,32 @@
+def remove_whitespace(original: str) -> str:
+    """
+    >>> remove_whitespace("I Love Python")
+    'ILovePython'
+    >>> remove_whitespace("I    Love       Python")
+    'ILovePython'
+    >>> remove_whitespace('    I     Love     Python')
+    'ILovePython'
+    >>> remove_whitespace("")
+    ''
+    """
+    return "".join(original.split())
+
+
+def remove_whitespace2(original: str) -> str:
+    """
+    >>> remove_whitespace2("I Love Python")
+    'ILovePython'
+    >>> remove_whitespace2("I    Love       Python")
+    'ILovePython'
+    >>> remove_whitespace2('    I     Love     Python')
+    'ILovePython'
+    >>> remove_whitespace2("")
+    ''
+    """
+    return original.replace(" ", "")
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
