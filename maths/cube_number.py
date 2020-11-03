4333a59216ae3b1d3ddb5a2ba740136b7970f119
shellhub <shellhub.me@gmail.com>
2020-11-03 17:52:31 +0800

    cube number

---
+++ maths/cube_number.py
@@ -0,0 +1,25 @@
+"""
+https://en.wikipedia.org/wiki/Cube_(algebra)
+"""
+
+
+def is_cube_number(number: int) -> bool:
+    """
+    >>> all(is_cube_number(num) for num in [-8, -1, 0, 1, 8, 27, 64, 8000, 216_000])
+    True
+    >>> is_cube_number(4)
+    False
+    >>> is_cube_number(11)
+    False
+    """
+    number = abs(number)
+    for i in range(0, number + 1):
+        if i ** 3 == number:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    from doctest import testmod
+
+    testmod()
