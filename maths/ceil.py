ec2970e32638d8fa52d9c4266f5cf7f6379fd502
shellhub <shellhub.me@gmail.com>
2020-11-04 09:07:49 +0800

    add ceil

---
+++ maths/ceil.py
@@ -0,0 +1,14 @@
+def ceil(number) -> int:
+    """
+    >>> import math
+    >>> numbers = [-3.14, 3.14, -3, 3, 0, 0.0, -0.0, -1234.567, 1234.567]
+    >>> all(math.ceil(num) == ceil(num) for num in numbers)
+    True
+    """
+    return int(number) if number - int(number) <= 0 else int(number) + 1
+
+
+if __name__ == '__main__':
+    from doctest import testmod
+
+    testmod()
