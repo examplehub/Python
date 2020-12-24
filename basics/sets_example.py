f313a58805f7d98feb84300fadf9a3d2318cb35b
Lewis Tian <taseikyo@gmail.com>
2020-12-24 15:25:20 +0800

    Add set examples

--- basics/sets_example.py
+++ basics/sets_example.py
@@ -9,6 +9,17 @@ def main() -> None:
     True
     >>> "c" in languages
     False
+    >>> my_langs = {"Python", "Golang", "C"}
+    >>> sorted(list(my_langs & languages))
+    ['C', 'Python']
+    >>> sorted(list(my_langs | languages))
+    ['C', 'Golang', 'Java', 'JavaScript', 'Python']
+    >>> sorted(list(my_langs ^ languages))
+    ['Golang', 'Java', 'JavaScript']
+    >>> sorted(list(my_langs - languages))
+    ['Golang']
+    >>> sorted(list(languages - my_langs))
+    ['Java', 'JavaScript']
     """
 
 
