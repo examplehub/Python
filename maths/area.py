f0c2a91d5c80e1057943422a1b4638c868cb59b8
Lewis Tian <taseikyo@gmail.com>
2021-02-19 19:26:59 +0800

    add more shapes (#33)

--- maths/area.py
+++ maths/area.py
@@ -10,6 +10,40 @@ def circle_area(radius: float) -> float:
     return math.pi * radius * radius
 
 
+def rectangle_area(length: float, width: float) -> float:
+    """
+    >>> rectangle_area(3, 4)
+    12
+    >>> rectangle_area(3, 0)
+    0
+    >>> rectangle_area(0, 4)
+    0
+    """
+    return length * width
+
+
+def square_area(length: float) -> float:
+    """
+    >>> square_area(4)
+    16
+    >>> square_area(0)
+    0
+    """
+    return length ** 2
+
+
+def triangle_area(length: float, height: float) -> float:
+    """
+    >>> triangle_area(3, 4)
+    6.0
+    >>> triangle_area(3, 0)
+    0.0
+    >>> triangle_area(0, 4)
+    0.0
+    """
+    return length * height / 2
+
+
 if __name__ == "__main__":
     from doctest import testmod
 
