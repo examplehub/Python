cd9dd9feb6dd65c2e0d545b5154a0f684e5c1e17
Du Yuanchao <shellhub.me@gmail.com>
2021-06-08 17:25:20 +0800

    add fibonacci (#35)
    * add fibonacci
    
    * Formatted with psf/black
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ maths/fibonacci_recursion.py
@@ -0,0 +1,32 @@
+def fibonacci(number: int) -> int:
+    """
+    >>> fibonacci(0)
+    0
+    >>> fibonacci(1)
+    1
+    >>> fibonacci(2)
+    1
+    >>> fibonacci(3)
+    2
+    >>> fibonacci(4)
+    3
+    >>> fibonacci(5)
+    5
+    >>> fibonacci(6)
+    8
+    >>> fibonacci(7)
+    13
+    >>> fibonacci(8)
+    21
+    """
+    return (
+        number
+        if number == 0 or number == 1
+        else fibonacci(number - 1) + fibonacci(number - 2)
+    )
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
